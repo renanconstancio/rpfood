ebeb55634bdbf3702cc3af5065dd3d258f6b6db8
renanconstancio <renanhconstancio@gmail.com>
2024-07-04 21:55:45 -0300

    bug: corrigir nome duplicado na tabela de usuarios

--- database/migrations/2021_05_12_053344_create_conversations_table.php
+++ database/migrations/2021_05_12_053344_create_conversations_table.php
@@ -6,33 +6,33 @@ use Illuminate\Support\Facades\Schema;
 
 class CreateConversationsTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-        Schema::create('conversations', function (Blueprint $table) {
-            $table->id();
-            $table->foreignId('sender_id')->nullable();
-            $table->string('sender_type')->nullable();
-            $table->foreignId('receiver_id')->default(0);
-            $table->string('receiver_type')->nullable();
-            $table->integer('unread_message_count')->nullable();
-            $table->foreignId('last_message_id')->nullable();
-            $table->dateTime('last_message_time')->nullable();
-            $table->timestamps();
-        });
-    }
+  /**
+   * Run the migrations.
+   *
+   * @return void
+   */
+  public function up()
+  {
+    Schema::create('conversations', function (Blueprint $table) {
+      $table->id();
+      $table->foreignId('sender_id')->nullable();
+      $table->string('sender_type')->nullable();
+      $table->foreignId('receiver_id')->default(0);
+      $table->string('receiver_type')->nullable();
+      $table->integer('unread_message_count')->nullable();
+      $table->foreignId('last_message_id')->nullable();
+      $table->dateTime('last_message_time')->nullable();
+      $table->timestamps();
+    });
+  }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        Schema::dropIfExists('conversations');
-    }
+  /**
+   * Reverse the migrations.
+   *
+   * @return void
+   */
+  public function down()
+  {
+    Schema::dropIfExists('conversations');
+  }
 }
